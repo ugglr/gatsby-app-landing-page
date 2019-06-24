91dbde38288d7ac0116b34735f60e44a74317e87
CarlW <carl@oskarroger.net>
2019-06-25 01:46:26 +0800

    Don't need Layout file. Adding bootstrap for responsive design abstraction.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/old/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
     <h1>Hi people, this is the index page.</h1>
-  </Layout>
+  </>
 )
 
 export default IndexPage

b7fcca02619e68b48180f290e331476039771d1c
CarlW <carl@oskarroger.net>
2019-06-25 02:41:07 +0800

    playing around with my design resources.

--- frontend/src/components/jumbo.js
+++ frontend/src/components/jumbo.js
@@ -1,13 +1,21 @@
 import React from "react"
 
+import bg from "../images/Component 1 – 1.svg"
+
 import { Jumbotron } from "react-bootstrap"
 
 export default function jumbo() {
   return (
     <Jumbotron
-      style={{ height: "100vh", maxWidth: "100%", overflow: "hidden" }}
+      className="jumbotron"
+      style={{
+        maxWidth: "100%",
+        overflow: "hidden",
+        padding: "0",
+      }}
     >
-      <div style={{ marginTop: "10rem" }}>
+      <img src={bg} style={{ width: "100%", zIndex: "-1" }} />
+      <div style={{ marginTop: "10rem", zIndex: "1" }}>
         <h1 className="text-center">The most awesome app</h1>
         <h4 className="text-center">In the world</h4>
       </div>

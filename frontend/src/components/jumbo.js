73f60d16b1f392ae813056f81934fe2cd7219297
CarlW <carl@oskarroger.net>
2019-06-27 00:54:35 +0800

    Fiddyling with the SVG Jumbo bg.

--- frontend/src/components/jumbo.js
+++ frontend/src/components/jumbo.js
@@ -1,21 +1,14 @@
 import React from "react"
 
-import bg from "../images/Component 1 – 1.svg"
-
 import { Jumbotron } from "react-bootstrap"
+
+//import CSS
+import "./jumbo.css"
 
 export default function jumbo() {
   return (
-    <Jumbotron
-      className="jumbotron"
-      style={{
-        maxWidth: "100%",
-        overflow: "hidden",
-        padding: "0",
-      }}
-    >
-      <img src={bg} style={{ width: "100%", zIndex: "-1" }} />
-      <div style={{ marginTop: "10rem", zIndex: "1" }}>
+    <Jumbotron className="jumbotron">
+      <div style={{ marginTop: "10rem" }}>
         <h1 className="text-center">The most awesome app</h1>
         <h4 className="text-center">In the world</h4>
       </div>

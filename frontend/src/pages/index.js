e089e2eaf4b38dd5e42c573e9e59fc78eaa63622
Ugglr <carl.igelstrom@gmail.com>
2019-06-27 16:24:51 +0800

    starting work on the navbar

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,11 +3,13 @@ import SEO from "../components/seo"
 
 import "./bootstrap.min.css"
 
+import Navbar from "../components/navbar"
 import Jumbo from "../components/jumbo"
 
 const IndexPage = () => (
   <>
     <SEO title="Home" />
+    <Navbar />
     <Jumbo />
   </>
 )

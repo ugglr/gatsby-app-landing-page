d7a46b1e713690c8038f6e61ac6813fe6ef25878
CarlW <carl@oskarroger.net>
2019-06-28 15:27:30 +0800

    working on the navbar.

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,5 +1,18 @@
 import React from "react"
 
+import "./navbar.css"
+
 export default function navbar() {
-  return <>hello from navbar</>
+  return (
+    <>
+      <div className="nav-container">
+        <ul className="nav-ul">
+          <li>Company Name</li>
+          <li className="float-right">Menu1</li>
+          <li className="float-right">Menu2</li>
+          <li className="float-right">Menu3</li>
+        </ul>
+      </div>
+    </>
+  )
 }
